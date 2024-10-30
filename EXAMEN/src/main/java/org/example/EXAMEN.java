5a41e2b92e2a881aa6512e335bca9ba5d54633c4
A607\1DAW <kikoperezcanto10@gmail.com>
2024-10-30 19:50:32 +0100

    examen und2 ej1

---
+++ EXAMEN/src/main/java/org/example/EXAMEN.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.Scanner;
+
+public class EXAMEN {
+
+    public  void ejercicio1 (){
+
+    }
+
+
+
+    public void ejercicio2 (){
+
+    }
+}
---
+++ EXAMEN/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        EXAMEN examen = new EXAMEN();
+        examen.ejercicio1();
+        examen.ejercicio2();
+
+
+    }
+
+}
